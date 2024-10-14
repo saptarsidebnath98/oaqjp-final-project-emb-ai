3f9e900f5f6c4076b5d916c01a8c8fbd0725bbb9
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-10-14 01:57:36 -0400

    response formatted as dict and added logic for extracting emotion scores and max score

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
-import requests  # Import the requests library to handle HTTP requests
+import requests, json  # Import the requests library to handle HTTP requests
+import numpy as np # Import Numpy
 
 def emotion_detector(text_to_analyze):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
 
@@ -10,4 +11,31 @@ def emotion_detector(text_to_analyze):  # Define a function named emotion_detect
 
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
 
-    return response.text  # Return the response text from the API
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    #extract the scores from the response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    # calculate maximum value and find dominant emotion
+    scores = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+    emotions = ['anger','disgust','fear','joy','sadness']
+
+    #getting max value index
+    max_index = np.argmax(scores)
+
+    #result dictionary output format
+    resultDict = {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': emotions[max_index]
+    }
+
+    return resultDict  # Return the response from the API
